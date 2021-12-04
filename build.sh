54dfdf9a2453171e7f8b9a3fe6923c1b07d9050f
Justin Zhang <schnell18@gmail.com>
2021-12-04 11:51:47 +0800

    Attempt using glibc to compile TiKV

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build -f Dockerfile-alpine -t schnell18/tikv-dev:v5.2.2 .
+# docker build -f Dockerfile-alpine -t schnell18/tikv-dev:v5.2.2 .
+docker build -f Dockerfile-debian -t schnell18/tikv-dev:v5.2.2 .
